80eddff639c8c3975a6b3a3d0c2e2cb485f828f8
ereninal10@gmail.com <ereninal10@gmail.com>
2020-03-10 22:40:08 +0300

    kodlar arduino megaya göre değiştirildi.

--- Arduino Code/otomat/otomat.ino
+++ Arduino Code/otomat/otomat.ino
@@ -1,14 +1,14 @@
 #include <LCD5110_Basic.h>
 #include <MFRC522.h>
 #include <SPI.h>
-int brightness = 0;
-int fadeAmount = 5;
-int resetPin = 9;
-int ssPin = 10;
-byte id[4]={0,0,0,0};
-MFRC522 rfid(ssPin,resetPin);
 
-LCD5110 myGLCD(8,7,6,5,4);
+
+int ResetPin = 49;
+int SDAPin = 53;
+byte id[4]={0,0,0,0};
+MFRC522 rfid(SDAPin,ResetPin);
+
+LCD5110 myGLCD(48,47,46,45,44);
 extern uint8_t SmallFont[];
 extern uint8_t MediumNumbers[];
 extern uint8_t BigNumbers[];
@@ -28,18 +28,18 @@ void loop() {
 
   
    
- LedYak(1000);
+ //LedYak(1000);
 if(!rfid.PICC_IsNewCardPresent())//yeni kart gelene kadar okuma yapan metot
  return;
  if(!rfid.PICC_ReadCardSerial())//kart oklutulmama durumunda çalışır
     return;
       myGLCD.clrScr();
-      ekranaYaz();
+      CardRead();
       rfid.PICC_HaltA();
      delay(5000);
       //delay()
 }
-void ekranaYaz(){
+void CardRead(){
   String numara;
   Serial.print("Id Numarasi: ");
   
@@ -53,7 +53,7 @@ void ekranaYaz(){
   myGLCD.print(numara,CENTER,16);
  
   Serial.println(" ");
-  Serial.print(numara);  
+  
 }
 void LedYak(int deger){
     analogWrite(3, 50);
