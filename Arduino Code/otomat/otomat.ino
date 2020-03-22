79dd93dd9d3b49eea03b6925bde770c72f0a3bb9
ereninal10@gmail.com <ereninal10@gmail.com>
2020-03-23 00:12:39 +0300

    Sd Card Modül Bağlandı, Rfid - Sd Card Çakışması Yaşandı.

--- Arduino Code/otomat/otomat.ino
+++ Arduino Code/otomat/otomat.ino
@@ -1,13 +1,13 @@
 #include <LCD5110_Basic.h>
 #include <MFRC522.h>
 #include <SPI.h>
-
+#include <SD.h>
 
 int ResetPin = 49;
 int SDAPin = 53;
 byte id[4]={0,0,0,0};
 MFRC522 rfid(SDAPin,ResetPin);
-
+File file;
 LCD5110 myGLCD(48,47,46,45,44);
 extern uint8_t SmallFont[];
 extern uint8_t MediumNumbers[];
@@ -21,26 +21,37 @@ void setup() {
   
   myGLCD.InitLCD();
   myGLCD.setFont(SmallFont);
-  pinMode(3,OUTPUT);
+  //pinMode(3,OUTPUT);
+   
+    /*if(!SD.begin(30)) {
+    Serial.println("SD Kart Takılı Değil!");
+     myGLCD.print("SD Kart Hata",CENTER,16);
+    return;
+  }*/
+ Serial.println("Cihaz Hazir");
+ myGLCD.print("Cihaz Hazir.",CENTER,16);
 }
 
 void loop() {
-
-  
-   
+   //CardRead();
  //LedYak(1000);
-if(!rfid.PICC_IsNewCardPresent())//yeni kart gelene kadar okuma yapan metot
- return;
+if(rfid.PICC_IsNewCardPresent()){
+ CardRead();//yeni kart gelene kadar okuma yapan metot
+ //return;
+  }
+ /*if(!rfid.PICC_IsNewCardPresent())
+  return;
  if(!rfid.PICC_ReadCardSerial())//kart oklutulmama durumunda çalışır
     return;
       myGLCD.clrScr();
-      CardRead();
+      CardRead();*/
       rfid.PICC_HaltA();
      delay(5000);
-      //delay()
+      //delay();
 }
 void CardRead(){
   String numara;
+  rfid.PICC_ReadCardSerial();
   Serial.print("Id Numarasi: ");
   
   for(int i=0;i<4;i++){
@@ -53,6 +64,7 @@ void CardRead(){
   myGLCD.print(numara,CENTER,16);
  
   Serial.println(" ");
+  Serial.println(numara);
   
 }
 void LedYak(int deger){
